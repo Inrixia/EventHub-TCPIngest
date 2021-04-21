cdd0ddacff1b82387bae3d84509e9a7ced5a3c7b
Inrix <Inrixia@gmail.com>
2021-04-21 18:53:45 +1200

    Added eventhub code & sent data to sanity check

--- src/ingest.ts
+++ src/ingest.ts
@@ -1,48 +1,79 @@
-import net from 'net';
-import http from 'http';
+import net from "net";
+import http from "http";
 
-let linesReceived = 0;
+import { EventHubProducerClient } from "@azure/event-hubs";
+
+const connectionString = "MDASandbox-eventHubs.servicebus.windows.net";
+const eventHubName = "kordiaraw";
+
+const producer = new EventHubProducerClient(connectionString, eventHubName);
+
+let receivedLines = 0;
 let receivedBytes = 0;
 let lastReceivedLine = "";
+
+let sentLines = 0;
+let sentBytes = 0;
+let lastSentLine = "";
 
 http.createServer((req, res) => {
 	res.write(JSON.stringify({
 		version: process.env.npm_package_version,
-		linesReceived: linesReceived-1,
-		lastReceivedLine,
-		receivedBytes
-	}))
+		received: {
+			lines: receivedLines-1,
+			bytes: receivedBytes,
+			lastLine: lastReceivedLine,
+		},
+		sent: {
+			lines: sentLines,
+			bytes: sentBytes,
+			lastLine: lastSentLine,
+		}
+	}));
 	res.end();
 }).listen(80);
 
-let dataBuffer = ""
+let lineBuffer = "";
+let lineBytes = 0;
 const ingestSocket = new net.Socket();
 
-ingestSocket.on('close', () => {
-	throw new Error('Connection closed')
-})
+ingestSocket.on("close", () => {
+	throw new Error("Connection closed");
+});
 
-ingestSocket.on('data', data => {
-	receivedBytes += data.byteLength
-	dataBuffer += data.toString();
-
+ingestSocket.on("data", async data => {
+	receivedBytes += data.byteLength;
+	lineBytes += data.byteLength;
+	lineBuffer += data.toString();
+	
 	let startIndex = 0;
 	let messageEndIndex = 0;
-	while ((messageEndIndex = dataBuffer.indexOf('\n', startIndex)) !== -1) {
-		linesReceived++
+	while ((messageEndIndex = lineBuffer.indexOf("\n", startIndex)) !== -1) {
+		const joinedLine = lineBuffer.slice(startIndex, messageEndIndex).replace("\n", "").replace("\r", "");
+		lastReceivedLine = joinedLine;
+		receivedLines++;
 
-		lastReceivedLine = dataBuffer.slice(startIndex, messageEndIndex).replace("\n", "").replace("\r", "")
+		if (receivedLines !== 1) {
+			// process.stdout.write(`${JSON.stringify(lastReceivedLine)}\nReceived: ${receivedLines-1}, ${(receivedBytes/1000/1000).toFixed(2)} MB\r`)
+
+			// Send the batch to the event hub.
+			await producer.sendBatch([{
+				body: joinedLine,
+				properties: {
+					receivedTime: Date.now()
+				}
+			}]).catch(console.error);
+			lastSentLine = joinedLine;
+			sentLines++;
+			sentBytes += lineBytes;
+		}
 
 		startIndex = messageEndIndex + 1;
-
-		// if (linesReceived !== 1) {
-		// 	process.stdout.write(`${JSON.stringify(lastReceivedLine)}\nReceived: ${linesReceived-1}, ${(receivedBytes/1000/1000).toFixed(2)} MB\r`)
-		// };
 	}
-	if (startIndex !== 0) dataBuffer = dataBuffer.slice(startIndex);
-})
+	if (startIndex !== 0) lineBuffer = lineBuffer.slice(startIndex);
+});
 
-const ip = '202.12.104.70';
+const ip = "202.12.104.70";
 const port = 4004;
 
 ingestSocket.connect(port, ip, () => console.log(`Connected to ingest tcp socket on ${ip}:${port}`));
