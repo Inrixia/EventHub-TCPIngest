8539d42c12ee69e378a6afff0a232b862cdd4bcd
Inrix <Inrixia@gmail.com>
2021-05-04 09:27:44 +1200

    Enabled eventhub ingestion & moved remaining options to env variables

--- src/ingest.ts
+++ src/ingest.ts
@@ -1,93 +1,82 @@
 import net from "net";
 import http from "http";
 
-// import { EventHubProducerClient } from "@azure/event-hubs";
-
+import { EventHubProducerClient } from "@azure/event-hubs";
 import { DefaultAzureCredential } from "@azure/identity";
-import { SecretClient } from "@azure/keyvault-secrets";
 
-const getSecret = async () => {
-	if (process.env["KEYVAULTURI"] === undefined) return "Enviroment variable KEYVAULTURI is undefined!";
-	const client = new SecretClient(process.env["KEYVAULTURI"], new DefaultAzureCredential());
-	return await client.getSecret("cake");
-};
+if (process.env["EVENTHUB_FULLYQUALIFIED_NAMESPACE"] === undefined) throw new Error("Enviroment variable EVENTHUB_FULLYQUALIFIED_NAMESPACE is undefined!");
+if (process.env["EVENTHUB_NAME"] === undefined) throw new Error("Enviroment variable EVENTHUB_NAME is undefined!");
+if (process.env["KORDIA_IP"] === undefined) throw new Error("Enviroment variable KORDIA_IP is undefined!");
+if (process.env["KORDIA_PORT"] === undefined) throw new Error("Enviroment variable KORDIA_PORT is undefined!");
 
-// const producer = new EventHubProducerClient(connectionString, eventHubName);
+const eventHubProducer = new EventHubProducerClient(process.env["EVENTHUB_FULLYQUALIFIED_NAMESPACE"], process.env["EVENTHUB_NAME"], new DefaultAzureCredential());
 
-http.createServer(async (req, res) => {
-	res.write(JSON.stringify(await getSecret().catch(err => {
-		if (err.name === "AggregateAuthenticationError") err.desc = "DefaultAzureCredential() failed to authenticate!";
-		return err;
-	})));
+const kordiaIP = process.env["KORDIA_IP"];
+const kordiaPort = process.env["KORDIA_PORT"];
+
+let receivedLines = 0;
+let receivedBytes = 0;
+let lastReceivedLine = "";
+
+let sentLines = 0;
+let sentBytes = 0;
+let lastSentLine = "";
+
+http.createServer((req, res) => {
+	res.write(JSON.stringify({
+		version: process.env.npm_package_version,
+		received: {
+			lines: receivedLines-1,
+			bytes: receivedBytes,
+			lastLine: lastReceivedLine,
+		},
+		sent: {
+			lines: sentLines,
+			bytes: sentBytes,
+			lastLine: lastSentLine,
+		}
+	}));
 	res.end();
 }).listen(80);
 
-// let receivedLines = 0;
-// let receivedBytes = 0;
-// let lastReceivedLine = "";
+let lineBuffer = "";
+let lineBytes = 0;
+const ingestSocket = new net.Socket();
 
-// let sentLines = 0;
-// let sentBytes = 0;
-// let lastSentLine = "";
+ingestSocket.on("close", () => {
+	throw new Error("Connection closed");
+});
 
-// http.createServer((req, res) => {
-// 	res.write(JSON.stringify({
-// 		version: process.env.npm_package_version,
-// 		received: {
-// 			lines: receivedLines-1,
-// 			bytes: receivedBytes,
-// 			lastLine: lastReceivedLine,
-// 		},
-// 		sent: {
-// 			lines: sentLines,
-// 			bytes: sentBytes,
-// 			lastLine: lastSentLine,
-// 		}
-// 	}));
-// 	res.end();
-// }).listen(80);
-
-// let lineBuffer = "";
-// let lineBytes = 0;
-// const ingestSocket = new net.Socket();
-
-// ingestSocket.on("close", () => {
-// 	throw new Error("Connection closed");
-// });
-
-// ingestSocket.on("data", async data => {
-// 	receivedBytes += data.byteLength;
-// 	lineBytes += data.byteLength;
-// 	lineBuffer += data.toString();
+ingestSocket.on("data", async data => {
+	receivedBytes += data.byteLength;
+	lineBytes += data.byteLength;
+	lineBuffer += data.toString();
 	
-// 	let startIndex = 0;
-// 	let messageEndIndex = 0;
-// 	while ((messageEndIndex = lineBuffer.indexOf("\n", startIndex)) !== -1) {
-// 		const joinedLine = lineBuffer.slice(startIndex, messageEndIndex).replace("\n", "").replace("\r", "");
-// 		lastReceivedLine = joinedLine;
-// 		receivedLines++;
+	let startIndex = 0;
+	let messageEndIndex = 0;
+	while ((messageEndIndex = lineBuffer.indexOf("\n", startIndex)) !== -1) {
+		const joinedLine = lineBuffer.slice(startIndex, messageEndIndex).replace("\n", "").replace("\r", "");
+		lastReceivedLine = joinedLine;
+		receivedLines++;
 
-// 		if (receivedLines !== 1) {
-// 			// process.stdout.write(`${JSON.stringify(lastReceivedLine)}\nReceived: ${receivedLines-1}, ${(receivedBytes/1000/1000).toFixed(2)} MB\r`)
+		if (receivedLines !== 1) {
+			// process.stdout.write(`${JSON.stringify(lastReceivedLine)}\nReceived: ${receivedLines-1}, ${(receivedBytes/1000/1000).toFixed(2)} MB\r`)
 
-// 			// Send the batch to the event hub.
-// 			await producer.sendBatch([{
-// 				body: joinedLine,
-// 				properties: {
-// 					receivedTime: Date.now()
-// 				}
-// 			}]).catch(console.error);
-// 			lastSentLine = joinedLine;
-// 			sentLines++;
-// 			sentBytes += lineBytes;
-// 		}
+			// Send the batch to the event hub.
+			await eventHubProducer.sendBatch([{
+				body: joinedLine,
+				properties: {
+					receivedTime: Date.now()
+				}
+			}]).catch(console.error);
+			lastSentLine = joinedLine;
+			sentLines++;
+			sentBytes += lineBytes;
+		}
 
-// 		startIndex = messageEndIndex + 1;
-// 	}
-// 	if (startIndex !== 0) lineBuffer = lineBuffer.slice(startIndex);
-// });
+		startIndex = messageEndIndex + 1;
+	}
+	if (startIndex !== 0) lineBuffer = lineBuffer.slice(startIndex);
+});
 
-// const ip = "202.12.104.70";
-// const port = 4004;
-
-// ingestSocket.connect(port, ip, () => console.log(`Connected to ingest tcp socket on ${ip}:${port}`));
+ingestSocket.connect(+kordiaPort, kordiaIP, () => console.log(`Connected to ingest tcp socket on ${kordiaIP}:${+kordiaPort}`));
