9f53f44895962a71a0fc74a5e40977daf5a39fa9
Inrix <Inrixia@gmail.com>
2021-05-04 12:01:59 +1200

    Fix bug in reported sentBytes & added queued meta

--- src/ingest.ts
+++ src/ingest.ts
@@ -11,25 +11,6 @@ if (process.env["KORDIA_PORT"] === undefined) throw new Error("Enviroment variab
 
 const eventHubProducer = new EventHubProducerClient(process.env["EVENTHUB_FULLYQUALIFIED_NAMESPACE"], process.env["EVENTHUB_NAME"], new DefaultAzureCredential());
 
-const messageQueue: { messages: Array<EventData>, bytes: number, lastLine: string } = {
-	messages: [],
-	bytes: 0,
-	lastLine: ""
-};
-const sendData = () => setTimeout(async () => {
-	if (messageQueue.messages.length !== 0) {
-		const messagesToSend = [...messageQueue.messages];
-		lastSentLine = messageQueue.lastLine;
-		sentLines += messageQueue.messages.length;
-		sentBytes += messageQueue.bytes;
-		messageQueue.messages = [];
-		messageQueue.bytes = 0;
-		await eventHubProducer.sendBatch(messagesToSend).catch(console.error);
-	}
-	sendData();
-});
-sendData();
-
 const kordiaIP = process.env["KORDIA_IP"];
 const kordiaPort = process.env["KORDIA_PORT"];
 
@@ -39,7 +20,24 @@ let lastReceivedLine = "";
 
 let sentLines = 0;
 let sentBytes = 0;
-let lastSentLine = "";
+
+let queuedBytes = 0;
+let queuedMessages: Array<EventData> = [];
+
+const sendData = () => setTimeout(async () => {
+	if (queuedMessages.length !== 0) {
+		sentBytes += queuedBytes;
+		queuedBytes = 0;
+
+		sentLines += queuedMessages.length;
+		// Copy and clear queuedMessages before sending so we dont clear messages that are added while sending
+		const linesToSend = [...queuedMessages];
+		queuedMessages = [];
+		await eventHubProducer.sendBatch(linesToSend).catch(console.error);
+	}
+	sendData();
+});
+sendData();
 
 http.createServer((req, res) => {
 	res.write(JSON.stringify({
@@ -49,10 +47,13 @@ http.createServer((req, res) => {
 			bytes: receivedBytes,
 			lastLine: lastReceivedLine,
 		},
+		queued: {
+			lines: queuedMessages.length,
+			bytes: queuedBytes
+		},
 		sent: {
 			lines: sentLines,
-			bytes: sentBytes,
-			lastLine: lastSentLine,
+			bytes: sentBytes
 		}
 	}));
 	res.end();
@@ -82,14 +83,14 @@ ingestSocket.on("data", async data => {
 			// process.stdout.write(`${JSON.stringify(lastReceivedLine)}\nReceived: ${receivedLines-1}, ${(receivedBytes/1000/1000).toFixed(2)} MB\r`)
 
 			// Send the batch to the event hub.
-			messageQueue.messages.push({
+			queuedMessages.push({
 				body: joinedLine,
 				properties: {
 					receivedTime: Date.now()
 				}
 			});
-			messageQueue.bytes += lineBytes;
-			messageQueue.lastLine = joinedLine;
+			queuedBytes += lineBytes;
+			lineBytes = 0;
 		}
 
 		startIndex = messageEndIndex + 1;
