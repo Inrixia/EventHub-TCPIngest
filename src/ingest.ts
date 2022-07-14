46a4f56d2fe88a9469c07762a22551c4d9b8615a
Inrix <Inrixia@gmail.com>
2022-07-14 12:43:42 +1200

    Removed manifests & http stats server. Updated packages

--- src/ingest.ts
+++ src/ingest.ts
@@ -1,5 +1,4 @@
 import net from "net";
-import http from "http";
 
 import { EventData, EventHubProducerClient } from "@azure/event-hubs";
 import { envOrThrow } from "@inrixia/helpers/object";
@@ -8,37 +7,19 @@ import { config } from "dotenv";
 config();
 
 (async () => {
-	const eventHubProducer = new EventHubProducerClient(await envOrThrow("EVENTHUB_CONNECTION_STRING"), envOrThrow("EVENTHUB_NAME"));
+	const eventHubProducer = new EventHubProducerClient(envOrThrow("EVENTHUB_CONNECTION_STRING"), envOrThrow("EVENTHUB_NAME"));
 
-	const tcpIP = await envOrThrow("TCP_IP");
-	const tcpPORT = +(await envOrThrow("TCP_PORT"));
+	const tcpIP = envOrThrow("TCP_IP");
+	const tcpPORT = +envOrThrow("TCP_PORT");
 
 	const prefixStation = process.env["STATION_PREFIX"];
 
-	const stats = {
-		version: process.env.npm_package_version,
-		received: {
-			lines: 0,
-			bytes: 0,
-			lastLine: "",
-		},
-		sent: {
-			lines: 0,
-			bytes: 0,
-		},
-		queued: {
-			lines: 0,
-			bytes: 0,
-		},
-	};
 	let queuedMessages: Array<EventData> = [];
+	let receivedLines = 0;
 
 	const sendData = () =>
 		setTimeout(async () => {
 			if (queuedMessages.length !== 0) {
-				stats.sent.bytes += stats.queued.bytes;
-				stats.sent.lines += queuedMessages.length;
-
 				// Copy and clear queuedMessages before sending so we dont clear messages that are added while sending
 				const linesToSend = [...queuedMessages];
 				queuedMessages = [];
@@ -46,23 +27,12 @@ config();
 					console.error(err);
 					process.exit();
 				});
-
-				stats.queued.lines = 0;
-				stats.queued.bytes = 0;
 			}
 			sendData();
 		}, 5000);
 	sendData();
 
-	http
-		.createServer((req, res) => {
-			res.write(JSON.stringify(stats));
-			res.end();
-		})
-		.listen(80);
-
 	let lineBuffer = "";
-	let lineBytes = 0;
 	const ingestSocket = new net.Socket();
 
 	ingestSocket.on("close", () => {
@@ -70,36 +40,29 @@ config();
 	});
 
 	ingestSocket.on("data", async (data) => {
-		stats.received.bytes += data.byteLength;
-		lineBytes += data.byteLength;
 		lineBuffer += data.toString();
 
 		let startIndex = 0;
 		let messageEndIndex = 0;
 		while ((messageEndIndex = lineBuffer.indexOf("\n", startIndex)) !== -1) {
 			let joinedLine = lineBuffer.slice(startIndex, messageEndIndex).replace("\n", "").replace("\r", "");
-			stats.received.lines++;
+			receivedLines++;
 
 			// Skip first message as its most likely incomplete, also filter only AIS messages
-			if (stats.received.lines !== 1 /*&& joinedLine.includes("AIVDM")*/) {
+			if (receivedLines !== 1 /*&& joinedLine.includes("AIVDM")*/) {
 				// Send the batch to the event hub.
 				if (prefixStation !== undefined) joinedLine = `\\s:${prefixStation}${joinedLine}`;
-				stats.received.lastLine = joinedLine;
 				queuedMessages.push({
 					body: joinedLine,
 					properties: {
 						receivedTime: Date.now(),
 					},
 				});
-				stats.queued.lines = queuedMessages.length;
-				stats.queued.bytes += lineBytes;
-				lineBytes = 0;
 			}
 
 			startIndex = messageEndIndex + 1;
 		}
 		if (startIndex !== 0) lineBuffer = lineBuffer.slice(startIndex);
 	});
-
 	ingestSocket.connect(tcpPORT, tcpIP, () => console.log(`Connected to ingest tcp socket on ${tcpIP}:${tcpPORT}`));
 })();
